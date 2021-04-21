da51d68927253c7a9167d377aabb2d906ca674e6
ThatColdToast <thatcoldtoast@gmail.com>
2021-04-20 19:57:59 -0500

    Initial commit

---
+++ src/main/java/thatcoldtoast/openglGame/Main.java
@@ -0,0 +1,77 @@
+package thatcoldtoast.openglGame;
+
+import thatcoldtoast.openglGame.graphics.Camera;
+import thatcoldtoast.openglGame.graphics.Mesh;
+import thatcoldtoast.openglGame.graphics.Shader;
+import thatcoldtoast.openglGame.graphics.Texture;
+import thatcoldtoast.openglGame.graphics.Transform;
+import thatcoldtoast.openglGame.io.Window;
+
+import static org.lwjgl.opengl.GL11.*;
+
+import org.joml.AxisAngle4f;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
+
+public class Main {
+	public static void main(String[] args) {
+		Window window = new Window();
+		
+		window.createWindow(1920, 1080);
+		
+		Mesh mesh = new Mesh();
+		mesh.create(new float[] {
+				-1,-1,0,  0,1,
+				0,1,0,    0.5f,0,
+				1,-1,0,   1,1
+		});
+		
+		Shader shader = new Shader();
+		shader.create("basic");
+		
+		Texture texture = new Texture();
+		texture.create("/textures/checker.png");
+		
+		Camera camera = new Camera();
+		Transform transform = new Transform();
+		
+		camera.setPerspective((float)Math.toRadians(70), 640.0f / 480.0f, 0.01f, 1000.0f);
+		camera.setPosition(new Vector3f(0, 1, 3));
+		camera.setRotation(new Quaternionf(new AxisAngle4f((float)Math.toRadians(-30), new Vector3f(1,0,0))));
+		
+		boolean isRunning = true;
+		
+		float x = 0;
+		
+		while (isRunning) {
+			isRunning = !window.update();
+			
+			x++;
+			transform.setPosition(new Vector3f((float)Math.sin(Math.toRadians(x)), 0, 0));
+			transform.getRotation().rotateAxis((float)Math.toRadians(1), 0, 1, 0);
+			
+			glClear(GL_COLOR_BUFFER_BIT);
+			
+			shader.useShader();
+			shader.setCamera(camera);
+			shader.setTransform(transform);
+			shader.setSampleTexture(0);
+			texture.bind();
+			mesh.draw();
+			
+			window.swapBuffers();
+			
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		texture.destroy();
+		mesh.destroy();
+		shader.destroy();
+		
+		window.free();
+	}
+}
---
+++ src/main/java/thatcoldtoast/openglGame/graphics/Shader.java
@@ -0,0 +1,130 @@
+package thatcoldtoast.openglGame.graphics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static org.lwjgl.opengl.GL11.GL_FALSE;
+import static org.lwjgl.opengl.GL20.*;
+
+public class Shader {
+	private int vertexShader, fragmentShader, program;
+	
+	private int uniMatProjection, uniMatTransformWorld, uniMatTransformObject;
+	private int uniSampleTexture;
+	
+	public Shader() {
+	}
+	
+	public boolean create(String shader) {
+		int success;
+		
+		vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertexShader, readSource(shader + ".vs"));
+		glCompileShader(vertexShader);
+		
+		success = glGetShaderi(vertexShader, GL_COMPILE_STATUS);
+		if (success == GL_FALSE) {
+			System.err.println("Vertex: \n" + glGetShaderInfoLog(vertexShader));
+			return false;
+		}
+		
+		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragmentShader, readSource(shader + ".fs"));
+		glCompileShader(fragmentShader);
+		
+		success = glGetShaderi(fragmentShader, GL_COMPILE_STATUS);
+		if (success == GL_FALSE) {
+			System.err.println("Fragment: \n" + glGetShaderInfoLog(fragmentShader));
+			return false;
+		}
+		
+		program = glCreateProgram();
+		glAttachShader(program, vertexShader);
+		glAttachShader(program, fragmentShader);
+		
+		glLinkProgram(program);
+		success = glGetProgrami(program, GL_LINK_STATUS);
+		if (success == GL_FALSE) {
+			System.err.println("Program Link: \n" + glGetProgramInfoLog(program));
+			return false;
+		}
+		glValidateProgram(program);
+		success = glGetProgrami(program, GL_VALIDATE_STATUS);
+		if (success == GL_FALSE) {
+			System.err.println("Program Validate: \n" + glGetProgramInfoLog(program));
+			return false;
+		}
+		
+		uniMatProjection = glGetUniformLocation(program, "cameraProjection");
+		uniMatTransformWorld = glGetUniformLocation(program, "transformWorld");
+		uniMatTransformObject = glGetUniformLocation(program, "transformObject");
+		uniSampleTexture = glGetUniformLocation(program, "sampleTexture");
+		
+		return true;
+	}
+	
+	public void destroy() {
+		glDetachShader(program, vertexShader);
+		glDetachShader(program, fragmentShader);
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+		glDeleteProgram(program);
+	}
+	
+	public void useShader() {
+		glUseProgram(program);
+	}
+	
+	public void setSampleTexture(int sample) {
+		if (uniSampleTexture != -1) {
+			glUniform1i(uniSampleTexture, sample);
+		}
+	}
+	
+	public void setCamera(Camera camera) {
+		if (uniMatProjection != -1) {
+			float matrix[] = new float[16];
+			camera.getProjection().get(matrix);
+			glUniformMatrix4fv(uniMatProjection, false, matrix);
+		}
+		if (uniMatTransformWorld != -1) {
+			float matrix[] = new float[16];
+			camera.getTransformation().get(matrix);
+			glUniformMatrix4fv(uniMatTransformWorld, false, matrix);
+		}
+	}
+	
+	public void setTransform(Transform transform) {
+		if (uniMatTransformObject != -1) {
+			float matrix[] = new float[16];
+			transform.getTransformation().get(matrix);
+			glUniformMatrix4fv(uniMatTransformObject, false, matrix);
+		}
+	}
+	
+	private String readSource(String file) {
+		BufferedReader reader = null;
+		StringBuilder sourceBuilder = new StringBuilder();
+		
+		try {
+			reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/shaders/" + file)));
+			
+			String line;
+			
+			while ((line = reader.readLine()) != null) {
+				sourceBuilder.append(line + "\n");
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return sourceBuilder.toString();
+	}
+}
